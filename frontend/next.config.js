d6d83236436759716db1866707da659ab5d16768
unknown <sushmitshrm@gmail.com>
2026-03-17 23:13:05 +0530

    remove CI workflow

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
     reactStrictMode: true,
     webpack: (config) => {
         config.resolve.fallback = { fs: false, net: false, tls: false };
@@ -16,5 +16,5 @@ const nextConfig: NextConfig = {
         ],
     },
 };
+module.exports = nextConfig
 
-export default nextConfig;
